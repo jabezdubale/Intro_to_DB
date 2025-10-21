9d7328545ad0be27708c7c39f904ba74bd8890a7
jabezdubale <jabezdubale6@gmail.com>
2025-10-21 11:50:52 +0300

    1. Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -21,7 +21,7 @@ except Error as e:
         )
 
         cursor = db.cursor()
-        cursor.execute("CREATE DATABASE alx_book_store")
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
         cursor.close
