04205f3fbe62a34c5303c76f8b306816baed069b
jabezdubale <jabezdubale6@gmail.com>
2025-10-21 11:05:35 +0300

    0. A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,7 +35,7 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id ),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
 
